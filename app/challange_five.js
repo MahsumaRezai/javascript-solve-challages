b0c99b6fbe95460acf82c12a8b79cf5aacdaa8bd
Masoumeh Rezai <you@example.com>
2023-03-28 03:20:35 +0430

    challange five

---
+++ app/challange_five.js
@@ -0,0 +1,7 @@
+//5. Calculate the sum of numbers from 1 to 10
+// answer
+let numbrs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let sum = numbrs.reduce((a, b) => {
+    return a + b;
+
+})
---
+++ app/challange_three.js
@@ -0,0 +1,6 @@
+// 3. Print the multiplication table with 7
+// answer
+for (let y = 1; y <= 10; y++) {
+    let m = y * 7;
+    document.write("<br>", m, "<br>")
+}
