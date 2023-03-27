6238f8adc72c7cc9f67b10887b5678f271c5ba11
Masoumeh Rezai <you@example.com>
2023-03-28 03:24:32 +0430

    challnge 9

---
+++ app/challange_nine.js
@@ -0,0 +1,10 @@
+// 9.Create a function that will convert from Fahrenheit to
+//Celsius
+// answer 
+function convert(c) {
+    return (c - 32) / 18;
+
+
+}
+let answer = convert(96);
+document.write(answer);
