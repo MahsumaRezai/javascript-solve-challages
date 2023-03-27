08553a0673e51c3462b195c811222ab47b35b33a
Masoumeh Rezai <you@example.com>
2023-03-28 03:23:31 +0430

    challange 8

---
+++ app/challange_eigth.js
@@ -0,0 +1,9 @@
+
+// 8 . Create a function that will convert from Celsius to Fahrenheit
+// answer 
+let fun = function (f) {
+    return f * 1.8 + 32;
+
+}
+let res = fun(40);
+document.write("<br>", res, "<br>")
