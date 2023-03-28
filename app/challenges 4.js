66dac19033d2ce2f80ab98bdbd681ad10242ada0
Masoumeh Rezai <you@example.com>
2023-03-29 03:07:00 +0430

    end parts one

---
+++ app/challenges 4.js
@@ -0,0 +1,9 @@
+//4. Print all the multiplication tables with numbers from 1 to 10
+
+function product(n) {
+    for (let i = 1; i <= 10; i++) {
+        let row = n + "*" + i + "=" + n * i;
+        document.write(row)
+    }
+
+} product();
