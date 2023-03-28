db36a9dac4bec47d5a44655f1b072a8ce01667cb
Masoumeh Rezai <you@example.com>
2023-03-29 02:28:46 +0430

    challenges 24

--- app/app.js
+++ app/app.js
@@ -4,19 +4,8 @@
 
 //4. Print all the multiplication tables with numbers from 1 to 10
 
-// 11. Calculate the average of the numbers in an array of numbers
-// answer 
 
-function aver() {
-    let arr = [1, 2, 3, 4, 5];
-    let n = arr.length;
-    let sum = 0;
-    for (let i = 0; i < n; i++) {
-        sum += arr[i]
-    }
-    return sum / n;
-}
-aver()
+
 // 12. Create a function that receives an array of numbers as argument and returns an
 //array containing only the positive numbers
 // answer 
@@ -44,6 +33,17 @@ alert("djffj")
 
 //19.Create a function that will return in an array the first “p” prime numbers
 //greater than “n”
+
+// 23.
+//24. Create a function that will merge two arrays and return the result as a new array
+function margearr() {
+    let arr1 = [1, 2, 3, 4, 5];
+    let arr2 = [6, 7, 8, 9, 10];
+    let concat = arr1.concat(arr2);
+    document.write(concat);
+
+} margearr();
+
 
 
 
