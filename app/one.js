61cb688a109eb59325f147c6611f002be1462ebe
Masoumeh Rezai <you@example.com>
2023-03-29 23:07:09 +0430

    add challenge 31

--- app/one.js
+++ app/one.js
@@ -1,3 +1,12 @@
+//31  Create a function that will return the number of words in a text
+// answer
+function word() {
+    let text = "Hello form the js code ";
+    let len = text.length;
+    document.write(len)
+}
+word();
+
 
 
 
