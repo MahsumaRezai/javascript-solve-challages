0f9053f833d9f05066c088581dc6d69d3bac0c91
Masoumeh Rezai <you@example.com>
2023-03-30 00:18:34 +0430

    add challenge 37

---
+++ app/challenge36.js
@@ -0,0 +1,11 @@
+// 36  Create a function that converts a string to an array of characters
+// answer 
+function arr() {
+    let str = "Hello";
+    for (let i = 0; i < str.length; i++) {
+        return Array.from(str)
+
+    }
+
+}
+arr()
--- app/one.js
+++ app/one.js
@@ -1,1 +1,14 @@
+// 37 
+function arr() {
+    let str = "Hello";
+    let ar = [];
+    for (let i = 0; i < str.length; i++) {
+        let code = str.charCodeAt();
+        ar.push(code)
 
+    }
+    return ar;
+
+
+}
+arr()
