d5f6945cc6d69e1d85f19988d887306f9feb6f39
Masoumeh Rezai <you@example.com>
2023-03-31 01:31:26 +0430

    work on chllange 40

---
+++ app/challenge40.js
@@ -0,0 +1,10 @@
+// 40.  Implement the bubble sort algorithm for an array of numbers
+
+// answer 
+function arr() {
+    let arr1 = [1, 2, 3, 45, 65];
+    let arr2 = arr1.sort();
+    let arr3 = arr2.reverse();
+    document.write(arr3)
+}
+arr()
--- app/one.js
+++ app/one.js
@@ -1,11 +1,3 @@
 // 38  Create a function that will convert an array containing ASCII codes in a string
 // answer 
 // 39
-// 40. 
-function arr() {
-    let arr1 = [1, 2, 3, 45, 65];
-    let arr2 = arr1.sort();
-    let arr3 = arr2.reverse();
-    document.write(arr3)
-}
-arr()
