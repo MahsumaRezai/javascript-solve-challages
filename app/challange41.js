e81531a57da2e647a4036dc6baec05338c587ddc
Masoumeh Rezai <you@example.com>
2023-03-31 01:43:14 +0430

    work on challange 41

---
+++ app/challange41.js
@@ -0,0 +1,10 @@
+//41  Create a function to calculate the distance between two points defined by their x, y coordinates
+
+function points(x1, x2, y1, y2) {
+    let p1 = x1 - x2;
+    let p2 = y1 - y2;
+    return (p1 / p2);
+
+
+}
+points();
--- app/one.js
+++ app/one.js
@@ -1,16 +1,7 @@
 // 38  Create a function that will convert an array containing ASCII codes in a string
 // answer 
 // 39
-//41  Create a function to calculate the distance between two points defined by their x, y coordinates
 
-function points(x1, x2, y1, y2) {
-    let p1 = x1 - x2;
-    let p2 = y1 - y2;
-    return (p1 / p2);
-
-
-}
-points();
 
 
 
