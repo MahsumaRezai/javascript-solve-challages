3ec50ff1f3c37eff026cae903c10ccf8b3dc71aa
Masoumeh Rezai <you@example.com>
2023-03-31 02:47:56 +0430

    work challenge 46

--- app/one.js
+++ app/one.js
@@ -3,7 +3,23 @@
 // 39
 // 42 
 // 43 
+// 46
+function maxvalue() {
+    let arr = [12, 13, 14[56], [90]];
+    let arr2 = arr.reduce((a, b) => {
+        if (a > b) {
+            return a;
 
+        }
+        else {
+            return b;
+        }
+
+    })
+    console.log(arr2)
+
+}
+maxvalue()
 
 
 
