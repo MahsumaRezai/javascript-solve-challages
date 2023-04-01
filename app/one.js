9c0d42715a8d49f7f4c0507b538e669d1c6084a2
Masoumeh Rezai <you@example.com>
2023-04-02 01:03:44 +0430

    work on challenge 48

--- app/one.js
+++ app/one.js
@@ -3,9 +3,20 @@
 // 38 39 
 // 42 
 
+//  48 
 
+function large() {
+    let word = "majdjdj";
+    for (let i = 0; i < word.length; i++) {
+        if (word.length >= 10) {
+            console.log("this is a lare word")
+        }
+        else if (word.length < 10) {
+            console.log("this is small word")
+        }
 
-
-
+    }
+}
+large()
 
 
